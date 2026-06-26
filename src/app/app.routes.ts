3ec8c0a6f48a196fb6a7233b4ebfde1f77c85e3b
Yahya250107 <rejayahya7@gmail.com>
2026-06-26 11:39:03 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,12 @@ import { DashboardComponent } from './Page/dashboard/dashboard';
 import { EmployeComponent } from './Page/employe/employe';
 import { LoginComponent } from './Page/login/login';
 import { MonSalaireComponent } from './Page/mon-salaire/mon-salaire';
-import { authGuard, managerGuard, rhGuard } from './Page/services/auth-guard';
+import { authGuard, managerGuard, rhGuard, employeeGuard } from './Page/services/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'salaires', component: SalaireComponent, canActivate: [authGuard, rhGuard] },
   { path: 'employes', component: EmployeComponent, canActivate: [authGuard, managerGuard] },
-  { path: 'mon-salaire', component: MonSalaireComponent, canActivate: [authGuard] },
+  { path: 'mon-salaire', component: MonSalaireComponent, canActivate: [authGuard, employeeGuard] },
 ];
